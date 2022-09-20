d0024f6a2fe22b3b8acf7872171ed4d1f88c94ed
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-09-20 19:25:30 +0100

    while loop

--- Day-06/loop.js
+++ Day-06/loop.js
@@ -53,3 +53,15 @@ for(let i = 0; i < newNumbers.length; i++) {
 }
 
 console.log(newArrNew)
+
+
+// ===> WHILE LOOP <====
+console.log(' ')
+console.log('===> WHILE LOOP <===')
+
+let i = 0
+while(i <= 9) {
+  console.log(i)
+  i++
+}
+// console.log(i)
