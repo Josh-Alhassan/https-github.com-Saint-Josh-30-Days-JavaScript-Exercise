28a01aec4c8f5de5dceea7891131510a86353019
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-09-20 19:09:02 +0100

    Adding elements into an empty array

--- Day-06/loop.js
+++ Day-06/loop.js
@@ -1,1 +1,44 @@
-console.log('Day 06')
+// for Loop
+
+// Incrementing
+for (let i = 0; i <=5; i++) {
+  console.log('increment ' + i)
+}
+
+console.log(' ')
+console.log('#####for Loop Decrement ######')
+// decrementing
+for (let i = 5; i >= 0; i--) {
+  console.log('dcrement ' + i)
+}
+
+console.log(' ')
+console.log('Multiplication Table with for Loop')
+
+for (let i = 0; i <=5; i++) {
+  console.log(`${i} * ${i} = ${i * i}`)
+}
+
+// Entering Items to a new array of strings
+console.log(' ')
+console.log('Pusing Array items into an empty Array')
+const countries = ['FinLand', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+const newArr = [];
+
+for(let i = 0; i < countries.length; i++) {
+  newArr.push(countries[i].toUpperCase())
+}
+console.log(newArr)
+
+// Adding all elements in the array
+console.log(' ')
+console.log('Adding all elements in an array')
+
+const numbers = [1,2,3,4,5];
+let sum = 0;
+for(let i = 0; i < numbers.length; i++) {
+  // console.log(numbers[i])
+  sum = sum + numbers[i]
+}
+console.log('Total number of element in array: ' +sum)
+
--- Day-06/loop.md
+++ Day-06/loop.md
@@ -1,2 +1,10 @@
 # Loops
 In programming langaugaes to cary out repetitive task we use different kinds of loops. The following are examples of loops commonly used in JavaScript
+
+### for loop
+```
+// For loop structure
+for (initialization, condition, increment/decrement) {
+  // code goes here
+}
+```
