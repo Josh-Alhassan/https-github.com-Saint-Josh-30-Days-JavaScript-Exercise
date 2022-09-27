da5d68c7d9f67c26e0dba678f5229d830c20fc36
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-09-27 08:09:40 +0100

    Global scope

--- Day-08/note.md
+++ Day-08/note.md
@@ -10,3 +10,5 @@ A *variable* can be declared at different **scope**. Variable scope can be
 ## Window Object
 Declaring a variable without let or const make it available in window object and this can be found anywhere.
 
+## Global scope
+A globally declared variable can be accessed every where in the same file. But the term global is reltive. It can be global to the file or it can be relative to some block of codes.
--- Day-08/objects.js
+++ Day-08/objects.js
@@ -1,7 +1,24 @@
+// ====> UTILITY FUNCTIONS <====
+
+// Space in the console function
+function spaceInConsole() {
+  console.log(' ')
+}
+
+// Title in console function
+function titleInConsole(title) {
+  let response = title;
+  console.log(response)
+}
+
+// ====> END OF UTILITY FUNCTIONS <====
+
 // Window global object
+titleInConsole('Window global object')
+
 a = 'JavaScript'
 b = 10
-
+ 
 function letsLearnScope () {
   console.log(a, b)
 
@@ -12,3 +29,26 @@ function letsLearnScope () {
 
 console.log(a, b) // Accessible
 letsLearnScope() // Accessible
+
+// Global Scope
+spaceInConsole()
+titleInConsole('Global Scope')
+
+/*
+  A global scope available anywhere in this file
+*/
+let c = 'JavaScript'; 
+let d = 10;
+
+function letsLearnGlobalScope() {
+  console.log(a, b) // accessible
+
+  if (true) {
+    let c = 'Python'
+    let b = 100
+    console.log(a, b) // Python 100
+  }
+  console.log(a, b)
+}
+letsLearnGlobalScope()
+console.log(a, b)
