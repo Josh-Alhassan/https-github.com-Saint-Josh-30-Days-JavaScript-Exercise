b319a06397445737b86035daccc9ccfe989f91d5
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-09-30 08:25:24 +0100

    Day 09 setup Higher order function

--- index.js
+++ index.js
@@ -24,8 +24,11 @@ import './style.css';
 // import './Day-07/functions.js'
 
 // Day 08 imports
-import './Day-08/objects.js'
-import './Day-08/exercise.js'
+import './Day-08/objects.js';
+import './Day-08/exercise.js';
+
+// Day 09 Imports
+import './Day-09/higherOrderFunc.js';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
@@ -33,15 +36,15 @@ appDiv.innerHTML = `<h1> 30 Days JavaScript</h1>`;
 
 // Question 1 - Write a single line comment which says, comments can make code readable
 // Answer:
-      // Comments can make code readble
+// Comments can make code readble
 
 // Question 2 - Write another single comment which says, Welcome to 30DaysOfJavaScript
 // Answer:
-      // Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
 
 // Question 3 - Write a multiline comment which says, comments can make code readable, easy to reuse and informative
 // Answer:
-    /*
+/*
       Comments can make code readable,
       easy to reuse and informative
     */
