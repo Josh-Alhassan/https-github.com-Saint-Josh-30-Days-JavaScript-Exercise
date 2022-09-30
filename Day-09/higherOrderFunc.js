0c970e5684924083ab8f018179b2f10efdc727fe
Saint-Josh <41088053+Saint-Josh@users.noreply.github.com>
2022-09-30 14:48:16 +0100

    returning functions

---
+++ Day-09/higherOrderFunc.js
@@ -0,0 +1,24 @@
+// Callback
+const callback = (n) => {
+  return n ** 2
+}
+
+// function that takes other function as a callback
+function cube(callback, n) {
+  return callback(n) * n
+}
+
+console.log(cube(callback, 3))
+
+// Returning function
+const higerOrder = n => {
+  const doSomething = m => {
+    const doWhatEver = t => {
+      return 2 * n + 3 * m + t
+    }
+    return doWhatEver
+  }
+  return doSomething
+}
+
+console.log(higherOder(2)(3)(10))
